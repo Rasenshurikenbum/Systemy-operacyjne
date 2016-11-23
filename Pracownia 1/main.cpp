fd64eb7fb7d2678dfe9e1807391098006935d73f
horrorschau105 <piotrekgdowski@wp.pl>
2016-11-23 16:30:41 +0100

    poprawka

--- Pracownia 1/main.cpp
+++ Pracownia 1/main.cpp
@@ -1,30 +1,25 @@
 //Krzysztof Lis, Piotr Gdowski
 //Pracownia P1
-
-/*Wybrany temat:
-Dwudziestu filozofów siedzi przy stole i każdy wykonuje jedną z dwóch czynności – albo je, albo rozmyśla. 
-Stół jest okrągły, przed każdym z nich znajduje się miska ze spaghetti, a pomiędzy każdą sąsiadującą parą 
-filozofów leży widelec, a więc każda osoba ma przy sobie dwie sztuki - po swojej lewej i prawej stronie. 
-Ponieważ jedzenie potrawy jest trudne przy użyciu jednego widelca, zakłada się, że każdy filozof korzysta 
-z dwóch. Dodatkowo nie ma możliwości skorzystania z widelca, który nie znajduje się bezpośrednio przed 
-daną osobą. Oprócz tego każdy filozof potrzebuje serwetki, aby kulturalnie mógł zjeść posiłek nie brudząc się.
-Na stole znajduje się 5 pudełek z dwoma serwetkami w każdym, po skończonym posiłku filozof oddaje serwetkę do pudełka.
-Do i-tego pudełka z serwetkami dostęp mają filozofowie 4(i-1)+1, 4(i-1)+2, 4(i-1)+3, 4(i-1)+4.
-
-Rozszerzenie: Zwiększenie liczby postaci i zasobów do 20 + manewry z serwetkami.*/
 #include <iostream>
+#include <unistd.h>
 #include <thread>
-
+#define N 20
 using namespace std;
-int __gcd(int a, int b)
+void nix(int n)
 	{
-	if(b==0) return a;
-	if(b>a) return __gcd(b,a);
-	return __gcd(b, a%b);
-}
+	cout << n << endl;
+	}
 int main()
 {
-	cout << "Hello world!\n";
-	for(int i=0;i<10;++i) cout << __gcd(4,i) << endl;
+	thread filozof[N]; // 20 watkow
+	for(int i=0;i<N;++i)
+	{
+	filozof[i] = thread(nix, i);
+	}
+	for(int i=0;i<N;++i)
+	{
+	filozof[i].join();	
+	}
+	cout << "\nEND\n";
 	return 0;
 }
