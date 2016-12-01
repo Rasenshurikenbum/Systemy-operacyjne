60ab06c8143fe239dda6f49e6ab981dd430f65fd
horrorschau105 <piotrekgdowski@wp.pl>
2016-12-01 15:44:27 +0100

    Adding makefile

---
+++ Pracownia 2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "threads.h"
+using namespace std;
+int main()
+{
+	cout << hello_world();
+}
---
+++ Pracownia 2/threads.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "threads.h"
+using namespace std;
+string hello_world()
+{
+	return "Hello World\n";
+}
